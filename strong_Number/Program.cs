d818aae7330fd1ef3eed28fae3625df38f613ddd
suhas <suhas@DELL>
2024-11-29 15:23:55 +0530

    Add project files.

---
+++ strong_Number/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strong_Number
+{
+    // sum of factorial of digite is equal to itself
+    //like 145=1!+4!+5!=145
+    class factorial
+    {
+        public static int fact(int n)
+        {
+            int f = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                f = f * i;
+            }
+            return f;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,sum=0;
+            Console.WriteLine("Enter the any number");
+            n=int.Parse(Console.ReadLine());
+            int tmp = n;
+            while(n>0)
+            {
+                int r = n % 10;
+                sum = sum + factorial.fact(r);
+                n = n / 10;
+                
+            }
+            if(sum==tmp)
+            {
+                Console.WriteLine("strong number");
+            }
+            else
+            {
+                Console.WriteLine("Not strong number");
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
